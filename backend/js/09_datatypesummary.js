d3b063d3175baee9229fa866d829f6c6ee83a363
Kushal9986 <rajputkushal9986@gmail.com>
2024-04-29 12:56:10 +0530

    datatype summary

---
+++ backend/js/09_datatypesummary.js
@@ -0,0 +1,15 @@
+//#primitive data types
+
+// 7 types : String , number , boolean , null (standalone value), undefined, Bigint
+
+//reference type(non primitive):
+//Array, Object , function
+
+const myarray = ['kushal','rajput'];
+let myobj = {
+    name : "kushal",
+    age  :  22,
+}
+const myfunction = function(){
+    
+}
