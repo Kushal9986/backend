e2b9314a8cd9e5ca291dfc049297064f668943d8
Kushal9986 <rajputkushal9986@gmail.com>
2024-04-29 10:54:16 +0530

    datatypes

---
+++ backend/js/05_datatypes.js
@@ -0,0 +1,19 @@
+//alert("kushal"); alert is allowed in browser only not in node
+
+let age = 22;
+let name = "kushal";
+let isloggedin = true;
+let height;//undefined
+let state = null;
+let unique = Symbol("@");
+
+console.log(typeof(unique));
+
+/*
+number/bigint = different ranges
+string = "" characters
+boolean = true/false
+null = object
+undefined = undefined
+unique = symbol
+*/
