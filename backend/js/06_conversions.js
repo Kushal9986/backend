a0b1d4d9dccaccc88f972b8b91ebe2db3f3916d9
Kushal9986 <rajputkushal9986@gmail.com>
2024-04-29 11:49:49 +0530

    conversion of data type

---
+++ backend/js/06_conversions.js
@@ -0,0 +1,37 @@
+
+//CONVERSION TO NUMBER
+let marks = "";
+
+let marksinNumber = Number(marks);
+//console.log(typeof (marksinNumber));
+//console.log(marksinNumber);
+
+/*other data types to numbers conversion
+// "33" => 33
+// "33abc"/undefined => NaN
+// true/false => 0/1
+//null/"" => 0*/
+
+
+let isloggedin = Symbol("k");
+let booleanIsloggedin = Boolean(isloggedin);
+//console.log(typeof (booleanIsloggedin));
+//console.log(booleanIsloggedin);
+
+
+/*non zero / zero => true / false
+"abc" / "" => true /? falsenu
+null / undefined => false
+symbol => true
+*/
+ let myage = true;
+
+ let stringMyage = String (myage);
+ console.log(stringMyage);
+ console.log(typeof(stringMyage));
+
+ /*
+ 33=> "33"
+ */
+
+
