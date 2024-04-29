4923e366c729dfb83565f16d1278f618df348598
Kushal9986 <rajputkushal9986@gmail.com>
2024-04-29 12:38:58 +0530

    comparison of data type

---
+++ backend/js/08_comparison.js
@@ -0,0 +1,19 @@
+// console.log(2 > 3);
+// console.log(2 >= 3);
+// console.log(2 < 3);
+// console.log(2 <= 3);
+// console.log(2 != 3);
+// console.log(2 == 3);
+
+//console.log("02" > 1);//true (convert to num)
+
+console.log(null > 0);//false as null converted to 0
+console.log(null >= 0 ); //true as null converted to 0 
+console.log(null == 0 ); //false as null is not converted
+
+console.log("2" == 2); //false "2" not converted
+
+/*
+equality check "==" and comparison check "< , > , <=, >= works differently 
+ equality check comparisons null to 0   "
+*/
