8b3f47c3da382d3b49ea2ccbe9a718cdbad3e831
Kushal9986 <rajputkushal9986@gmail.com>
2024-04-29 12:18:22 +0530

    operations

---
+++ backend/js/07_operation.js
@@ -0,0 +1,21 @@
+//not imp
+// console.log(2 + 3);
+// console.log(2 * 3);
+// console.log(2 / 3);
+// console.log(2 - 3);
+// console.log(2 % 3);
+
+
+//IMPORTANT
+
+let str1 = "kushal";
+let str2 = "rajput";
+let str3 = str1 + str2;
+
+console.log(1 + "2");//12
+console.log("1" + 2);//12
+console.log(1 +  2 + "2");//32
+console.log("1" + 2 + 2);//122
+
+
+
