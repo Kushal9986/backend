b5a6f9ea9c3cb94cc83a6a771c89e8efe273fa49
Kushal9986 <rajputkushal9986@gmail.com>
2024-04-30 11:39:45 +0530

    string details

---
+++ backend/js/11_stringdetails.js
@@ -0,0 +1,24 @@
+let myname = "kushal";
+let myage = 22;
+
+//console.log(`${myname} is ${myage} years old`);//for readability
+
+let myname2 = new String ("kushal-rajput ");//using string class
+//console.log(myname2.__proto__);//check methods in browser console
+
+// console.log(myname2.toUpperCase());
+// console.log(myname2.concat(2));
+// console.log(myname2.indexOf('h'));
+// console.log(myname2);
+
+let myurl  = new String("    https://kushalrajput%20     .com       ");
+
+
+//replace, include, split, trim, substr, slice
+// console.log(myurl.replace("%20","-"));
+// console.log(myurl.includes(".om"));
+// console.log(myurl.includes(".om"));
+// console.log(myurl.trim());remove spaces from both ends
+//console.log(myurl.substring(0,14));doesn't includes last index
+// console.log(myurl.slice(0,14));can accept negative values for reverse start
+//console.log(`character code of  ${myurl.charAt(5)}  is ${myurl.charCodeAt(5)}`);
