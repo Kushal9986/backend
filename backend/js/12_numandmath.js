cd5fc0a7a03a8cb42e1109de5eea8c4030ade102
Kushal9986 <rajputkushal9986@gmail.com>
2024-05-15 18:31:00 +0530

    number and maths

---
+++ backend/js/12_numandmath.js
@@ -0,0 +1,24 @@
+const age = 22;
+
+const age1 = new Number(22);
+// console.log(age1);
+// console.log(age);
+
+// console.log(age1.toString().length);
+const num = 12846.4;
+
+//console.log(age.toFixed(4));
+
+//console.log(num.toPrecision(6));
+
+// console.log(num.toLocaleString('en-IN'));
+
+
+//-------------Maths--------
+// console.log(Math.abs(-83));//83
+//  console.log(Math.round(4.5));//4
+// console.log(Math.ceil(4.9));//5
+// console.log(Math.floor(4.9));//4
+// console.log(Math.min(4,6,2,9));//2
+console.log(Math.random());//returns value always between o to 1
+console.log((Math.random() *10) +1);
